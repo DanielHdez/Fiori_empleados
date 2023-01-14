6f5c2866468e393ff6b4b283eda66719ca48a790
DanielHdez <dhdezleon@gmail.com>
2023-01-14 21:11:13 +0000

    Comenzando programa

--- empleados/webapp/controller/MainView.controller.js
+++ empleados/webapp/controller/MainView.controller.js
@@ -1,3 +1,4 @@
+
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
@@ -6,10 +7,53 @@ sap.ui.define([
      */
     function (Controller) {
         "use strict";
+       /* function myCheck() {
+            var inputEmployee = this.byId("inputemployee");
+            var valueEmployee = inputEmployee.getValue();
 
-        return Controller.extend("dhl.empleados.controller.MainView", {
+            if (valueEmployee.length === 6) {
+                inputEmployee.setDescription("OK");
+            } else {
+                inputEmployee.setDescription("NO OK");
+            };
+        }*/
+        /*return Controller.extend("dhl.empleados.controller.MainView", {
             onInit: function () {
 
+            },
+             //onvalidate: myCheck
+             onvalidate: function(){
+                var inputEmployee = this.byId("inputemployee");
+                var valueEmployee = inputEmployee.getValue();
+    
+                if (valueEmployee.length === 6) {
+                    inputEmployee.setDescription("OK");
+                    this.byId("labelcountry").setVisible(true);
+                    this.byId("slcountry").setVisible(true);
+                } else {
+                  inputEmployee.setDescription("NO OK");
+                  this.byId("labelcountry").setVisible(false);
+                  this.byId("slcountry").setVisible(false);
+                };
             }
         });
+    });
+*/
+            var Main = Controller.extend("dhl.empleados.controller.MainView", {});
+            /*onvalidate: myCheck*/
+            Main.prototype.onvalidate = function(){
+                var inputEmployee = this.byId("inputemployee");
+                var valueEmployee = inputEmployee.getValue();
+    
+                if (valueEmployee.length === 6) {
+                    inputEmployee.setDescription("OK");
+                    this.byId("labelcountry").setVisible(true);
+                    this.byId("slcountry").setVisible(true);
+                } else {
+                  inputEmployee.setDescription("NO OK");
+                  this.byId("labelcountry").setVisible(false);
+                  this.byId("slcountry").setVisible(false);
+                }
+        };
+        return Main;
     });
